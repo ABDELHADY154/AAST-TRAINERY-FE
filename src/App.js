ca586d8dae1152110055495e64d55274be4177d9
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-10-03 18:28:59 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-basmaaaaaaa
-    </div>
-  );
+  return <div className="App">Life Labour</div>;
 }
 
 export default App;
