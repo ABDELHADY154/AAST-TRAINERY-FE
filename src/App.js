e0e0dbc5c84074f4a5b025a2d8f6badec2839f32
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-11-07 22:40:29 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Life Labour</div>;
+  return <div className="App">Mohamed abdelhady elshamy</div>;
 }
 
 export default App;
