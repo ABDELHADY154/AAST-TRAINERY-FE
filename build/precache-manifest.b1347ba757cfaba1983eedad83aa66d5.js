af0a30a6505d8da479a8f4124a861d262bfc1a9b
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-11-07 22:44:38 +0200

    .

---
+++ build/precache-manifest.b1347ba757cfaba1983eedad83aa66d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260f1673013cb074559e727a1b42b2fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acd00104e8856633c1f1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ea09ff9d1393da4e325",
+    "url": "/static/js/2.b6932a07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6932a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd00104e8856633c1f1",
+    "url": "/static/js/main.3c7227bf.chunk.js"
+  },
+  {
+    "revision": "37bda2bde2813034c90c",
+    "url": "/static/js/runtime-main.2932611f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
