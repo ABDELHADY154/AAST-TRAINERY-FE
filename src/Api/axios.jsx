b9a8389cee509e651df29edd2b89840dd1c094c4
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-02-19 21:05:22 +0200

    .

--- src/Api/axios.jsx
+++
@@ -1,9 +0,0 @@
-
-import Axios from "axios";
-
-export const axios = Axios.create({
-  baseURL: "http://admin.aast-trainery.com/api",
-});
-
-axios.defaults.headers.common["Authorization"] =
-  "Bearer " + sessionStorage.getItem("token");
