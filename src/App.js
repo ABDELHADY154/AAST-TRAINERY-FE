7e6195f2f4598585144b15341752760e491ecfa5
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-02-19 20:22:35 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Registry from "./Components/Registry";
 import Home from "./Components/Home";
@@ -23,7 +25,7 @@ const CheckAuth = () => {
 const AuthRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
-    render={() => (CheckAuth() ? <Component /> : <Redirect to='/Login' />)}
+    render={() => (CheckAuth() ? <Component /> : <Redirect to="/Login" />)}
   />
 );
 
@@ -42,45 +44,50 @@ class App extends React.Component {
     }
   }
 
-  setUser = (data) => {
+  setUser = data => {
     return this.setState({ loggedIn: data });
   };
   render() {
     // console.log(this.state.loggedIn);
     return (
-      <BrowserRouter>
-        <Nav loggedIn={this.state.loggedIn} setUser={this.setUser} />
-        <div className='app'>
-          <div className='auth-wrapper'>
-            <div className='auth-inner'>
-              <Switch>
-                {/* <== Home for Guest */}
+      <Router>
+        <BrowserRouter>
+          <Nav loggedIn={this.state.loggedIn} setUser={this.setUser} />
+          <div className="app">
+            <div className="auth-wrapper">
+              <div className="auth-inner">
+                <Switch>
+                  {/* <== Home for Guest */}
 
-                <Route
-                  exact
-                  path='/'
-                  component={() => <Landing loggedIn={this.state.loggedIn} />}
-                />
-                {/* <Route exact path='/'>
+                  <Route
+                    exact
+                    path="/"
+                    component={() => <Landing loggedIn={this.state.loggedIn} />}
+                  />
+                  {/* <Route exact path='/'>
                   <Redirect to='/Login' />
                 </Route> */}
-                <Route path='/Login' component={() => <Login setUser={this.setUser} />} />
-                <Route path='/Register' component={Registry} />
-                <Route path='/Forget' component={Forget} />
+                  <Route
+                    path="/Login"
+                    component={() => <Login setUser={this.setUser} />}
+                  />
+                  <Route path="/Register" component={Registry} />
+                  <Route path="/Forget" component={Forget} />
 
-                {/* //component={() => <Login isLoggedin={this.isLoggedin} />} */}
-                {/* <== Home for Users */}
-                <AuthRoute exact path='/Home' component={Home} />
-                {/* <== Register Guests */}
+                  {/* //component={() => <Login isLoggedin={this.isLoggedin} />} */}
+                  {/* <== Home for Users */}
+                  <AuthRoute exact path="/Home" component={Home} />
+                  {/* <== Register Guests */}
 
-                <AuthRoute exact path='/Profile' component={Profile} />
+                  <AuthRoute exact path="/Profile" component={Profile} />
 
-                <Route exact path='*' component={ErrorPage} />
-              </Switch>
+                  <Route exact path="*" component={ErrorPage} />
+                </Switch>
+              </div>
             </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Router>
     );
   }
 }
