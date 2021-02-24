7dea00a7386a5a11341cbc37f85acfbb84638909
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-02-25 00:28:37 +0200

    .

---
+++ build/precache-manifest.70b98d8d83ec1a02e8b6d5d03453d5bb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b572bb7faadc53171254f52aefe61bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb5e65adad3cc045c59b",
+    "url": "/static/css/2.52f2db83.chunk.css"
+  },
+  {
+    "revision": "1f28029fa4cced5d168e",
+    "url": "/static/css/main.0810531b.chunk.css"
+  },
+  {
+    "revision": "bb5e65adad3cc045c59b",
+    "url": "/static/js/2.12566cf4.chunk.js"
+  },
+  {
+    "revision": "64c44d1cefc3299cbd4358bd0ccbe7e6",
+    "url": "/static/js/2.12566cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f28029fa4cced5d168e",
+    "url": "/static/js/main.70a7b16f.chunk.js"
+  },
+  {
+    "revision": "b5b3c431a9f23cabde82",
+    "url": "/static/js/runtime-main.ae909c23.js"
+  },
+  {
+    "revision": "4db7d35d21f5e22b1f8a76e95e6c7cd5",
+    "url": "/static/media/2.4db7d35d.png"
+  },
+  {
+    "revision": "09d1b4ef329857e6f7aa74f56dbaca5b",
+    "url": "/static/media/Main-Logo.09d1b4ef.png"
+  },
+  {
+    "revision": "9cf232894f373c497e9c3f177332ea1e",
+    "url": "/static/media/Rectangle1.9cf23289.png"
+  },
+  {
+    "revision": "ac7bc01521caddd54a5622a9d88167f7",
+    "url": "/static/media/Rectangle2.ac7bc015.png"
+  },
+  {
+    "revision": "0c81c9b4821c07ab3ba7b1444cb4e7df",
+    "url": "/static/media/Rectangle3.0c81c9b4.png"
+  },
+  {
+    "revision": "25fc1714fcfa6a3fd50d8442a6610be0",
+    "url": "/static/media/SF-Compact-Display-Bold.25fc1714.otf"
+  },
+  {
+    "revision": "75066b22f9ab74333180dc8ecc787a1b",
+    "url": "/static/media/SF-Compact-Display-Light.75066b22.otf"
+  },
+  {
+    "revision": "892258a37b8e592b0226d8e482957541",
+    "url": "/static/media/SF-Compact-Display-Medium.892258a3.otf"
+  },
+  {
+    "revision": "f472479ec6d07136098c3c0f707496cc",
+    "url": "/static/media/SF-Compact-Display-Regular.f472479e.otf"
+  },
+  {
+    "revision": "03843dfb6d4edacbe6c30b5175644681",
+    "url": "/static/media/bg1.03843dfb.png"
+  },
+  {
+    "revision": "7f280aa5de11be8216acb2dd8a581e95",
+    "url": "/static/media/bg12.7f280aa5.png"
+  },
+  {
+    "revision": "90382eada935ed790075800dd2b28028",
+    "url": "/static/media/bg13.90382ead.png"
+  },
+  {
+    "revision": "b564cba4d928fa99d0b929b2a79100ab",
+    "url": "/static/media/bg2.b564cba4.png"
+  },
+  {
+    "revision": "531690aeeb2a09fa694f9a27049d0253",
+    "url": "/static/media/boyavi.531690ae.png"
+  },
+  {
+    "revision": "82c0c269083557386986d79fb0fae073",
+    "url": "/static/media/done2.82c0c269.svg"
+  },
+  {
+    "revision": "f39702458d98aab95d08046421ab9ff5",
+    "url": "/static/media/error.f3970245.png"
+  },
+  {
+    "revision": "cc9443500678c9e55f52ccb5ce14a96a",
+    "url": "/static/media/girlavi.cc944350.png"
+  },
+  {
+    "revision": "fea36ab5bb2ab74e0f2468e44363220f",
+    "url": "/static/media/google.fea36ab5.png"
+  },
+  {
+    "revision": "3a2646fd7046c7066e826d855cad3428",
+    "url": "/static/media/login.3a2646fd.jpg"
+  },
+  {
+    "revision": "47e9d19d6de022c3d73670deb013abfe",
+    "url": "/static/media/logo.47e9d19d.png"
+  },
+  {
+    "revision": "7ec060b9cd0988469264d588d7a01d0c",
+    "url": "/static/media/rec1.7ec060b9.png"
+  },
+  {
+    "revision": "8e53cc8d83a167b53861c47cbd42e209",
+    "url": "/static/media/rec2.8e53cc8d.png"
+  },
+  {
+    "revision": "4bc4a10f66c3e8da142b49ccf143668c",
+    "url": "/static/media/rec3.4bc4a10f.png"
+  }
+]);
