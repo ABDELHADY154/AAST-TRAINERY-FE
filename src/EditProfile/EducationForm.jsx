b6d96ef5d3f0f36046c0938e45a10b28f84df144
Me8oOo <68943037+Me8oOo@users.noreply.github.com>
2021-03-02 22:28:41 +0200

    .

--- src/EditProfile/EducationForm.jsx
+++ src/EditProfile/EducationForm.jsx
@@ -1,13 +1,96 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 // import { Redirect } from "react-router-dom";
 // import { axios } from "../Api/axios";
 // import { Link } from "react-router-dom";
+import {
+  CountryDropdown,
+  RegionDropdown,
+  CountryRegionData,
+} from "react-country-region-selector";
 
 class EducationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { country: "", region: "" };
+  }
+  state = {
+    startDate: new Date(),
+  };
+  selectCountry(val) {
+    this.setState({ country: val });
+  }
+
+  selectRegion(val) {
+    this.setState({ region: val });
+  }
+  handleChange = (startDate) => {
+    this.setState({
+      startDate,
+    });
+  };
   render() {
+    const { country, region } = this.state;
+
     return (
-      <div className="container text-center">
-        <h1>Education </h1>
+      <div className='Education'>
+        <form class='text-start g-3 '>
+          <div class='col-md-8'>
+            <label for='validationServer03' class='form-label'>
+              City <span className='bg-red'> *</span>
+            </label>
+            <input
+              type='text'
+              class='form-control is-invalid'
+              id='validationServer03'
+              aria-describedby='validationServer03Feedback'
+              required
+            />
+            <div id='validationServer03Feedback' class='invalid-feedback'>
+              Please provide a valid city.
+            </div>
+          </div>
+          <div className='row g-3'>
+            <div class='col-md-4'>
+              <label for='validationServer04' class='form-label'>
+                State
+              </label>{" "}
+              <CountryDropdown
+                value={country}
+                onChange={(val) => this.selectCountry(val)}
+                class='form-select'
+                id='validationServer04'
+                aria-describedby='validationServer04Feedback'
+                required
+              />
+            </div>
+            <div class='col-md-4'>
+              <label for='validationServer05' class='form-label'>
+                Zip
+              </label>
+              <RegionDropdown
+                country={country}
+                value={region}
+                onChange={(val) => this.selectRegion(val)}
+                class='form-select is-invalid'
+                id='validationServer04'
+                aria-describedby='validationServer04Feedback'
+                required
+              />
+            </div>
+            <div class='input-group date' data-provide='datepicker'>
+              <input type='text' class='form-control' />
+              <div class='input-group-addon'>
+                <span class='glyphicon glyphicon-th'></span>
+              </div>
+            </div>
+
+            <div class='col-12'>
+              <button class='btn btn-primary' type='submit'>
+                Submit form
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
--- src/EditProfile/GeneralForm.jsx
+++ src/EditProfile/GeneralForm.jsx
@@ -6,16 +6,16 @@ import React, { Component } from "react";
 class GeneralForm extends Component {
   render() {
     return (
-      <div className="container text-center">
-        <div class="col-12">
-          <label for="inputFullname" class="form-label">
+      <div className='container text-center'>
+        <div class='col-8'>
+          <label for='inputFullname' class='form-label'>
             Fullname
           </label>
           <input
-            type="text"
-            class="form-control"
-            id="inputFullname"
-            placeholder="Fullname"
+            type='text'
+            class='form-control'
+            id='inputFullname'
+            placeholder='Fullname'
           />
         </div>
       </div>
