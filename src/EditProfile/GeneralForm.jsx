bd6e070d66d344035abe5a061e12760e49d71240
Basma Mostafa <57597830+basmaamostafa@users.noreply.github.com>
2021-03-03 17:05:25 +0200

    general form

--- src/EditProfile/GeneralForm.jsx
+++ src/EditProfile/GeneralForm.jsx
@@ -20,25 +20,38 @@ class GeneralForm extends Component {
   render() {
     return (
       <form class="row g-3 mb-3">
-        <label class="form-label" for="customFile">
-          Default file input example
-        </label>
-        <div>
-          <input type="file" onChange={this.handleChange} />
-          <img src={this.state.file} />
+        <div class="col-11 mb-4">
+          <div className="row imgCol">
+            <img src={this.state.file} className="col-2 profieImg" />
+            <div className="col-10">
+              <label class="form-label" for="customFile">
+                Profile Photo
+              </label>
+              <p>
+                You can upload a .jpg, .png, or .gif photo with max size of
+                10MB.
+              </p>
+              <input
+                type="file"
+                className="imgUpload shadow-none "
+                accept="image/x-png,image/gif,image/jpeg"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
         </div>
-        <div class="col-12">
+        <div class="col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12">
           <label for="inputfullname" class="form-label editLabel">
             Full Name<span className="text-danger ms-2">*</span>
           </label>
           <input
             type="text"
-            className="form-control editInput longInput"
+            className="form-control editInput "
             id="fullname"
             placeholder="Please enter your full name"
           />
         </div>
-        <div class="col-md-6">
+        <div class="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12">
           <label for="inputEmail4" className="form-label editLabel">
             Gender<span className="text-danger ms-2">*</span>
           </label>
@@ -75,129 +88,122 @@ class GeneralForm extends Component {
             </div>
           </div>
         </div>
-        <div className="col-md-6 halfEdit">
+        <div className="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12 ">
           <label for="inputDOB" className="form-label editLabel ">
             Date of birth<span className="text-danger ms-2">*</span>
           </label>
-          <input
-            type="date"
-            className="form-control editInput halfInput "
-            id="DOB"
-          />
+          <input type="date" className="form-control editInput  " id="DOB" />
         </div>
-        <div className="col-12">
+        <div className="ol-lg-10 col-11 col-md-10 col-sm-12 col-xs-12">
           <label for="inputNationaity" class="form-label editLabel ">
             Nationaity<span className="text-danger ms-2">*</span>
           </label>
           <input
             type="text"
-            className="form-control editInput longInput "
+            className="form-control editInput  "
             id="nationaity"
             placeholder="Please enter your Nationaity"
           />
         </div>
-        <div className="col-md-6">
+        <div className="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12">
           <label for="inputCountry" className="form-label editLabel">
             Country<span className="text-danger ms-2">*</span>
           </label>
-          <select
-            id="inputCountry"
-            className="form-select editInput halfInput "
-          >
+          <select id="inputCountry" className="form-select editInput  ">
             <option selected>Choose your Country...</option>
             <option>...</option>
           </select>
         </div>
-        <div className="col-md-6 halfEdit">
+        <div className="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12 ">
           <label for="inputCity" className="form-label editLabel">
             City<span className="text-danger ms-2">*</span>
           </label>
-          <select id="inputCity" className="form-select editInput halfInput ">
+          <select id="inputCity" className="form-select editInput  ">
             <option selected>Choose your City...</option>
             <option>...</option>
           </select>
         </div>
-        <div className="col-12">
+        <div className="col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12">
           <label for="inputPhone" class="form-label editLabel">
             Phone Number<span className="text-danger ms-2">*</span>
           </label>
           <input
             type="number"
-            className="form-control editInput longInput"
+            className="form-control editInput "
             id="phone"
             placeholder="Please enter your Phone Number"
           />
         </div>
-        <div className="col-md-6 ">
+        <div className="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12 ">
           <label for="inputuni" className="form-label editLabel">
             University / Institution<span className="text-danger ms-2">*</span>
           </label>
-          <select id="inputuni" className="form-select editInput halfInput ">
+          <select id="inputuni" className="form-select editInput  ">
             <option selected>Choose your University / Institution ...</option>
             <option>...</option>
           </select>
         </div>
-        <div className="col-md-6 halfEdit">
+        <div className="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12 ">
           <label for="inputDep" className="form-label editLabel">
             Field of study / Department
             <span className="text-danger ms-2">*</span>
           </label>
-          <select id="inputDep" className="form-select editInput  halfInput ">
+          <select id="inputDep" className="form-select editInput   ">
             <option selected>Choose your Field of study / Department...</option>
             <option>...</option>
           </select>
         </div>
-        <div className="col-12">
+        <div className="col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12">
           <label for="inputRegNum" class="form-label editLabel">
             Registration Number<span className="text-danger ms-2">*</span>
           </label>
           <input
             type="number"
-            className="form-control editInput longInput"
+            className="form-control editInput "
             id="RegNum"
             placeholder="Please enter your Registration Number"
           />
         </div>
-        <div class="col-md-6">
+        <div class="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12">
           <label for="inputTerm" className="form-label editLabel">
             Term<span className="text-danger ms-2">*</span>
           </label>
-          <select id="inputTerm" className="form-select editInput halfInput">
+          <select id="inputTerm" className="form-select editInput ">
             <option selected>Choose your Term ...</option>
             <span className="text-danger ms-2">*</span>
             <option>...</option>
           </select>
         </div>
-        <div class="col-md-6 halfEdit">
+        <div class="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12 ">
           <label for="inputGPA" className="form-label editLabel">
             Grade / GPA<span className="text-danger ms-2">*</span>
           </label>
-          <select id="inputGPA" className="form-select editInput halfInput">
+          <select id="inputGPA" className="form-select editInput ">
             <option selected>Choose your Grade / GPA...</option>
             <option>...</option>
           </select>
         </div>
-        <div class="col-md-6">
+        <div class="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12">
           <label for="bdaymonth" className="form-label editLabel">
             Start Year<span className="text-danger ms-2">*</span>
           </label>
           <input
             type="month"
             id="bdaymonth"
-            className="form-control editInput halfInput"
+            className="form-control editInput "
           />
         </div>
-        <div class="col-md-6 halfEdit">
+        <div class="col-lg-5 col-11 col-md-5 col-sm-12 col-xs-12 ">
           <label for="bdaymonth" className="form-label editLabel">
             Expected end Year<span className="text-danger ms-2">*</span>
           </label>
           <input
             type="month"
             id="bdaymonth"
-            className="form-control editInput halfInput "
+            className="form-control editInput  "
           />
         </div>
-        <div class="col-12 d-flex justify-content-end">
+        <div class="col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12 d-flex justify-content-end mt-5">
           <button type="submit" class="btn me-2 cancelBtn shadow-none">
             Cancel
           </button>
@@ -205,7 +211,7 @@ class GeneralForm extends Component {
             Add
           </button>
         </div>
-        <div class="col-12 d-flex justify-content-end">
+        <div class="col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12 d-flex justify-content-end">
           <button type="submit" class="btn deleteBtn me-2 shadow-none ">
             Delete
           </button>
