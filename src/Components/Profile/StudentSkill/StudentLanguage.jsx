85a4b70bf99746f7466a700602c6c4f4f144478c
Yasmin <yazzmin0011@gmail.com>
2021-03-07 19:33:03 +0200

    Revert "Merge branch 'master' into Feature-ViewProfilePage_Yasmin-Sabry"
    This reverts commit fc789e54a9154bd342004b9e7add97c5de4c840e, reversing
    changes made to 3640206fb7deadce8af7113c7e09e9b81c620029.

--- src/Components/Profile/StudentSkill/StudentLanguage.jsx
+++ src/Components/Profile/StudentSkill/StudentLanguage.jsx
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../../../layout/Profile.css";
 import { BiPencil } from "react-icons/bi";
-export default class StudentEdu extends Component {
-  constructor(props) {
-    super(props);
-  }
+import ReactStars from "react-rating-stars-component";
+
+export default class StudentWorkExp extends Component {
   render() {
-    let id = this.props.id;
     return (
       <>
         <div
@@ -15,31 +13,56 @@ export default class StudentEdu extends Component {
           className="d-flex flex-row  fs-5 "
           style={{ textTransform: "capitalize" }}
         >
-          <div className=" d-flex flex-row col-10 col-md-11 me-1">
-            {this.props.course_provider}
+          <div className=" d-flex flex-row flex-wrap col-4 col-md-5 me-1">
+            {this.props.skill_name}
+          </div>
+          <div
+            id="worktag"
+            className=" d-flex flex-row flex-wrap col-3 col-md-1  fs-6 "
+          >
+            {this.props.worktag}
+          </div>
+          <div
+            id="worktag"
+            className=" d-flex flex-row flex-wrap col-3 col-md-5 "
+          >
+            <ReactStars
+              count={5}
+              value={4}
+              edit={false}
+              size={28}
+              activeColor="#F2A23A"
+            />
           </div>
           <div
             id="hiddenhover"
             className=" d-flex flex-row col-2 col-md-1 p-0 ms-3"
           >
-            <Link renderAs="button" to={`Profile/Courses/${id}`}>
+            <Link
+              renderAs="button"
+
+              // to="/Register"
+            >
               <BiPencil fill="#cd8930" color="#cd8930" />
             </Link>
           </div>
         </div>
         <div className="d-flex flex-row fs-6 ">
           <div className=" d-flex flex-row col-12 col-md-12">
-            {this.props.course_name}.
+            {this.props.workCity}, {this.props.workCountry}.
           </div>
         </div>
         <div className="d-flex flex-row fs-6 ">
-          <div className=" d-flex flex-row col-12 col-md-12"></div>
+          <div className=" d-flex flex-row col-12 col-md-12">
+            {this.props.fromWork} to {this.props.toWork} ·
+            {/* {diffDays} Years */}
+          </div>
         </div>
         <div className="d-flex flex-row fs-6 mt-2 ">
           <a
             id="goldcredentials"
             renderAs="button"
-            href={this.props.cred_url}
+            href={this.props.workCredential_url}
             className="  mb-1   d-flex flex-row col-12 col-md-3"
             // to="/Register"
           >
