d90b64616a37342ca89413684d5b525e0f1c3c78
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-03-08 21:54:07 +0200

    Revert "Merge pull request #81 from ABDELHADY154/Feature-StudentCourseInfo_mostafa-fathy-mohamed"
    This reverts commit 33a87e1fd0df8218d61a7aa9f139f0fad061fa1d, reversing
    changes made to df1e501fc7f89163f18f7a65e94b6df2aa71958a.

--- src/Components/Profile/StudentCourses/StudentCourses.jsx
+++ src/Components/Profile/StudentCourses/StudentCourses.jsx
@@ -3,38 +3,44 @@ import { Link } from "react-router-dom";
 import "../../../layout/Profile.css";
 import { BiPencil } from "react-icons/bi";
 export default class StudentEdu extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     let id = this.props.id;
     return (
       <>
         <div
-          id='lightfont'
-          className='d-flex flex-row  fs-5 '
+          id="lightfont"
+          className="d-flex flex-row  fs-5 "
           style={{ textTransform: "capitalize" }}
         >
-          <div className=' d-flex flex-row col-10 col-md-11 me-1'>
-            {this.props.courseProviderName}
+          <div className=" d-flex flex-row col-10 col-md-11 me-1">
+            {this.props.course_provider}
           </div>
-          <div id='hiddenhover' className=' d-flex flex-row col-2 col-md-1 p-0 ms-3'>
-            <Link renderAs='button' to={`Profile/Courses/${id}`}>
-              <BiPencil fill='#cd8930' color='#cd8930' />
+          <div
+            id="hiddenhover"
+            className=" d-flex flex-row col-2 col-md-1 p-0 ms-3"
+          >
+            <Link renderAs="button" to={`Profile/Courses/${id}`}>
+              <BiPencil fill="#cd8930" color="#cd8930" />
             </Link>
           </div>
         </div>
-        <div className='d-flex flex-row fs-6 '>
-          <div className=' d-flex flex-row col-12 col-md-12'>
-            {this.props.courseName}.
+        <div className="d-flex flex-row fs-6 ">
+          <div className=" d-flex flex-row col-12 col-md-12">
+            {this.props.course_name}.
           </div>
         </div>
-        <div className='d-flex flex-row fs-6 '>
-          <div className=' d-flex flex-row col-12 col-md-12'></div>
+        <div className="d-flex flex-row fs-6 ">
+          <div className=" d-flex flex-row col-12 col-md-12"></div>
         </div>
-        <div className='d-flex flex-row fs-6 mt-2 '>
+        <div className="d-flex flex-row fs-6 mt-2 ">
           <a
-            id='goldcredentials'
-            renderAs='button'
-            href={this.props.courseCredential_url}
-            className='  mb-1   d-flex flex-row col-12 col-md-3'
+            id="goldcredentials"
+            renderAs="button"
+            href={this.props.cred_url}
+            className="  mb-1   d-flex flex-row col-12 col-md-3"
             // to="/Register"
           >
             See credentials
