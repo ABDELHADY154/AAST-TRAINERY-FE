2aeeaddcb2394ebe90b43c1774fe13501bb9fb4c
Yasmin <yazzmin0011@gmail.com>
2021-03-08 10:07:03 +0200

    .

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -15,6 +15,8 @@ import studentReviews from "./StudentReviews/StudentReviews";
 
 import studentAccount from "./StudentAccount/StudentAccount";
 import Footer2 from "../Common/Footer2";
+import img2 from "../../Components/assests/imgs/cib.png";
+import "../../layout/Home.css";
 
 // import { Redirect } from "react-router-dom";
 import { axios } from "../../Api/axios";
@@ -427,17 +429,43 @@ class Profile extends Component {
           <div id="education" className="card mt-5">
             <div className="card-body">
               <div className="d-flex flex-row titlecard fs-4 mb-2">Reviews</div>
-              <div className="carousel" data-flickity='{ "autoPlay": true }'>
-                {/* {this.state.reviews.map((item) => {
-                  return (
-                    <studentReviews
-                    // key={item.id}
-                    // courseProviderName={item.school_name}
-                    // courseName={item.courseName}
-                    // Coursecred={item.courseCredential_url}
-                    />
-                  );
-                })} */}
+              <div
+                id="carouselExampleControls"
+                className="carousel slide"
+                data-bs-ride="carousel"
+              >
+                <div className="carousel-inner ">
+                  <div className="carousel-item active">
+                    <div className="flex-row d-flex ">
+                      <div className="col-md-12">
+                        <div className="card">
+                          <div className="card-body">
+                            <div className="d-flex flex-row">
+                              <img
+                                className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
+                                // id="imgicon"
+                                src={img2}
+                              />
+                              <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
+                                UI/UX Designer
+                              </div>
+                            </div>
+                            <div id="job" className="d-flex flex-row ms-5 ">
+                              <div className="d-flex ms-3 flex-column">CIB</div>
+                            </div>
+                            <p className="card-text mt-2">
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. Ipsam repudiandae aut possimus. Repellendus
+                              at nostrum iste doloremque. Ea omnis ipsam, eum
+                              nam tempore culpa illum consequuntur quis nobis
+                              adipisci et?
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -448,3 +476,15 @@ class Profile extends Component {
   }
 }
 export default Profile;
+{
+  /* {this.state.reviews.map((item) => {
+                  return (
+                    <studentReviews
+                    // key={item.id}
+                    // courseProviderName={item.school_name}
+                    // courseName={item.courseName}
+                    // Coursecred={item.courseCredential_url}
+                    />
+                  );
+                })} */
+}
--- src/Components/Profile/StudentAccount/StudentAccount.jsx
+++ src/Components/Profile/StudentAccount/StudentAccount.jsx
@@ -1,42 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "../../../layout/carousel.css";
 import "../../../layout/Profile.css";
-import img2 from "../../../Components/assests/imgs/cib.png";
 export default class StudentAccount extends Component {
   render() {
-    return (
-      <>
-        <div className="carousel-cell">
-          <div className="flex-row d-flex ">
-            <div className="col-md-12">
-              <div className="card">
-                <div className="card-body">
-                  <div className="d-flex flex-row">
-                    <img
-                      className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
-                      id="imgicon"
-                      src={img2}
-                    />
-                    <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
-                      UI/UX Designer
-                    </div>
-                  </div>
-                  <div id="job" className="d-flex flex-row ms-5 ">
-                    <div className="d-flex ms-3 flex-column">CIB</div>
-                  </div>
-                  <p className="card-text mt-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ipsam repudiandae aut possimus. Repellendus at nostrum iste
-                    doloremque. Ea omnis ipsam, eum nam tempore culpa illum
-                    consequuntur quis nobis adipisci et?
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
+    return <></>;
   }
 }
