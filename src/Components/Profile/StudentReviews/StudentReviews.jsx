29c0d35feb16f5682fefdc2000e62e23c422c530
Yasmin <yazzmin0011@gmail.com>
2021-03-11 14:13:00 +0200

    hotfix pre trial to install caro card package

--- src/Components/Profile/StudentReviews/StudentReviews.jsx
+++ src/Components/Profile/StudentReviews/StudentReviews.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../../../layout/Profile.css";
 import img2 from "../../../Components/assests/imgs/cib.png";
-
-export default class studentReviews extends Component {
+export default class StudentAccount extends Component {
   render() {
     return (
       <>
-        {/* <div
+        <div
           id="carouselExampleControls"
           className="carousel slide"
           data-bs-ride="carousel"
@@ -15,63 +14,55 @@ export default class studentReviews extends Component {
           <div className="carousel-inner ">
             <div className="carousel-item active">
               <div className="flex-row d-flex ">
-                <div className="d-flex flex-column col-md-12">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="d-flex flex-row">
-                        <img
-                          className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
-                          // id="imgicon"
-                          src={img2}
-                        />
-                        <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
-                          UI/UX Designer
-                        </div>
-                      </div>
-                      <div id="job" className="d-flex flex-row ms-5 ">
-                        <div className="d-flex ms-3 flex-column">CIB</div>
-                      </div>
-                      <p className="card-text mt-2">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ipsam repudiandae aut possimus. Repellendus at nostrum
-                        iste doloremque. Ea omnis ipsam, eum nam tempore culpa
-                        illum consequuntur quis nobis adipisci et?
-                      </p>
+                <div className="col-md-12">
+                  <div className="d-flex flex-row">
+                    <img
+                      className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
+                      // id="imgicon"
+                      src={img2}
+                    />
+                    <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
+                      UI/UX Designer
                     </div>
                   </div>
+                  <div id="job" className="d-flex flex-row ms-5 ">
+                    <div className="d-flex ms-3 flex-column">CIB</div>
+                  </div>
+                  <p className="card-text mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ipsam repudiandae aut possimus. Repellendus at nostrum iste
+                    doloremque. Ea omnis ipsam, eum nam tempore culpa illum
+                    consequuntur quis nobis adipisci et?
+                  </p>
                 </div>
               </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleControls"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleControls"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
             </div>
-
-            <div className="carousel-item active">
-              <img src={img2} className="d-block w-100" alt="..." />
-            </div>
-            <button
-              class="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button
-              class="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Next</span>
-            </button>
           </div>
-        </div> */}
+        </div>
       </>
     );
   }
