b5b048bab5b773b64ba0efb2297d642c197b2374
Basma Mostafa <57597830+basmaamostafa@users.noreply.github.com>
2021-04-22 23:36:00 +0200

    hotfix

--- src/Components/StudentActivity/Applied.jsx
+++ src/Components/StudentActivity/Applied.jsx
@@ -5,6 +5,7 @@ import ActivityNavbar from "./Navbar";
 import img2 from "../../Components/assests/imgs/cib.png";
 import { BsArrowUpRight } from "react-icons/bs";
 import Footer2 from "../Common/Footer2";
+import { BsBookmark } from "react-icons/bs";
 
 import "../../layout/EditInfo.css";
 class Applied extends Component {
@@ -14,199 +15,73 @@ class Applied extends Component {
         <Link to="/Profile/Activity/Applied" />
         <div className="container">
           <ActivityNavbar setactive={"Applied"} />
-          <div className="card cardMt">
-            <div className="card-body">
-              <div className="d-flex flex-row">
-                <img
-                  className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
-                  id="imgicon"
-                  src={img2}
-                />
-                <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
-                  UI/UX Designer
-                </div>
-                <div id="goldtab" className=" fs-6 mt-2 col-2 col-md-1">
-                  Paid
-                </div>
-              </div>
-              <div id="job" className="d-flex flex-row ms-5 ">
-                <div className="d-flex ms-3 flex-column">CIB</div>
-                <div id="gold" className="  d-flex ms-2 flex-column">
-                  Finance
-                </div>
-              </div>
-              <p className="card-text mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-                repudiandae aut possimus. Repellendus at nostrum iste
-                doloremque. Ea omnis ipsam, eum nam tempore culpa illum
-                consequuntur quis nobis adipisci et?
-              </p>
 
-              {
-                <div className="d-flex flex-row flex-wrap rowHeight ">
-                  <div
-                    className="d-flex flex-column  col-4 col-md-1 me-4 mrcss "
-                    id="firsttagipad"
-                  >
-                    <a href="#" className="tagsipad" id="tags">
-                      Finance
-                    </a>
-                  </div>
-                  <div
-                    className="d-flex flex-column col-4  col-md-1 me-4 mb-1 mrcss "
-                    id="firsttagipad"
-                  >
-                    <a href="#" className="tagsipad  " id="tags">
-                      Banking
-                    </a>
-                  </div>
-                  <div
-                    id="drop"
-                    className="d-flex flex-column col-md-2 col-9 
-                             justify-space-between mrcss textWidth"
-                  >
-                    <p>Deadline {"        "}11 Dec 2021</p>
-                  </div>
-                  <div className=" mb-4 d-flex flex-row col-12 col-md-2 justify-content-start me-1">
-                    <BsArrowUpRight
-                      className="me-2"
-                      color="#cd8930"
-                      fill="#cd8930"
+          <div className="row mb-3">
+            <div className="col-md-12">
+              <div className="card">
+                <div className="card-body">
+                  <div className="d-flex flex-row">
+                    <img
+                      className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
+                      id="imgicon"
+                      src={img2}
                     />
-                    <p id="gold">Promoted</p>
+                    <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
+                      UI/UX Designer
+                    </div>
+                    <div id="goldtab" className=" fs-6 mt-2 col-2 col-md-1">
+                      Paid
+                    </div>
                   </div>
-                </div>
-              }
-            </div>
-          </div>
-          <div className="card cardMt">
-            <div className="card-body">
-              <div className="d-flex flex-row">
-                <img
-                  className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
-                  id="imgicon"
-                  src={img2}
-                />
-                <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
-                  UI/UX Designer
-                </div>
-                <div id="goldtab" className=" fs-6 mt-2 col-2 col-md-1">
-                  Paid
-                </div>
-              </div>
-              <div id="job" className="d-flex flex-row ms-5 ">
-                <div className="d-flex ms-3 flex-column">CIB</div>
-                <div id="gold" className="  d-flex ms-2 flex-column">
-                  Finance
-                </div>
-              </div>
-              <p className="card-text mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-                repudiandae aut possimus. Repellendus at nostrum iste
-                doloremque. Ea omnis ipsam, eum nam tempore culpa illum
-                consequuntur quis nobis adipisci et?
-              </p>
-
-              {
-                <div className="d-flex flex-row flex-wrap rowHeight ">
-                  <div
-                    className="d-flex flex-column  col-4 col-md-1 me-4 mrcss "
-                    id="firsttagipad"
-                  >
-                    <a href="#" className="tagsipad" id="tags">
+                  <div id="job" className="d-flex flex-row ms-5 ">
+                    <div className="d-flex ms-3 flex-column">CIB</div>
+                    <div id="gold" className="  d-flex ms-2 flex-column">
                       Finance
-                    </a>
+                    </div>
                   </div>
-                  <div
-                    className="d-flex flex-column col-4  col-md-1 me-4 mb-1 mrcss "
-                    id="firsttagipad"
-                  >
-                    <a href="#" className="tagsipad  " id="tags">
-                      Banking
-                    </a>
+                  <p className="card-text mt-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ipsam repudiandae aut possimus. Repellendus at nostrum iste
+                    doloremque. Ea omnis ipsam, eum nam tempore culpa illum
+                    consequuntur quis nobis adipisci et?
+                  </p>
+                  <div className="d-flex flex-row flex-wrap ">
+                    <div
+                      className="d-flex flex-column  col-4 col-md-1  "
+                      id="firsttagipad"
+                    >
+                      <a href="#" className="tagsipad" id="tags">
+                        Finance
+                      </a>
+                    </div>
+                    <div
+                      className="d-flex flex-column col-4  col-md-1 mb-1 "
+                      id="firsttagipad"
+                    >
+                      <a href="#" className="tagsipad  " id="tags">
+                        Banking
+                      </a>
+                    </div>
                   </div>
-                  <div
-                    id="drop"
-                    className="d-flex flex-column col-md-2 col-9 
-                             justify-space-between mrcss textWidth"
-                  >
-                    <p>Deadline {"        "}11 Dec 2021</p>
+                  <div className="row mt-1">
+                    <div
+                      id="drop"
+                      className="d-flex flex-column col-md-5  
+                           "
+                    >
+                      <p>Deadline {"        "}11 Dec 2021</p>
+                    </div>
+                    <div className=" d-flex flex-row col-12 col-md-2 justify-content-start me-1">
+                      <BsArrowUpRight
+                        className="me-2"
+                        color="#cd8930"
+                        fill="#cd8930"
+                      />
+                      <p id="gold">Promoted</p>
+                    </div>
                   </div>
-                  <div className=" mb-4 d-flex flex-row col-12 col-md-2 justify-content-start me-1">
-                    <BsArrowUpRight
-                      className="me-2"
-                      color="#cd8930"
-                      fill="#cd8930"
-                    />
-                    <p id="gold">Promoted</p>
-                  </div>
-                </div>
-              }
-            </div>
-          </div>
-          <div className="card mt-3 mb-5">
-            <div className="card-body">
-              <div className="d-flex flex-row">
-                <img
-                  className=" mt-0 d-flex flex-column col-md-1 col-2 me-1"
-                  id="imgicon"
-                  src={img2}
-                />
-                <div className=" fs-5 mt-2 ms-2 col-md-10 col-8">
-                  UI/UX Designer
-                </div>
-                <div id="goldtab" className=" fs-6 mt-2 col-2 col-md-1">
-                  Paid
                 </div>
               </div>
-              <div id="job" className="d-flex flex-row ms-5 ">
-                <div className="d-flex ms-3 flex-column">CIB</div>
-                <div id="gold" className="  d-flex ms-2 flex-column">
-                  Finance
-                </div>
-              </div>
-              <p className="card-text mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-                repudiandae aut possimus. Repellendus at nostrum iste
-                doloremque. Ea omnis ipsam, eum nam tempore culpa illum
-                consequuntur quis nobis adipisci et?
-              </p>
-
-              {
-                <div className="d-flex flex-row flex-wrap ">
-                  <div
-                    className="d-flex flex-column  col-4 col-md-1 me-4 "
-                    id="firsttagipad"
-                  >
-                    <a href="#" className="tagsipad" id="tags">
-                      Finance
-                    </a>
-                  </div>
-                  <div
-                    className="d-flex flex-column col-4  col-md-1 me-4 mb-1 "
-                    id="firsttagipad"
-                  >
-                    <a href="#" className="tagsipad  " id="tags">
-                      Banking
-                    </a>
-                  </div>
-                  <div
-                    id="drop"
-                    className="d-flex flex-column col-md-3  
-                             justify-space-between"
-                  >
-                    <p>Deadline {"        "}11 Dec 2021</p>
-                  </div>
-                  <div className=" mb-4 d-flex flex-row col-12 col-md-2 justify-content-start me-1">
-                    <BsArrowUpRight
-                      className="me-2"
-                      color="#cd8930"
-                      fill="#cd8930"
-                    />
-                    <p id="gold">Promoted</p>
-                  </div>
-                </div>
-              }
             </div>
           </div>
         </div>
--- src/Components/StudentActivity/Appointment.jsx
+++ src/Components/StudentActivity/Appointment.jsx
@@ -6,6 +6,7 @@ import rec3 from "../../Components/assests/imgs/rec3.png";
 import { BsArrowUpRight } from "react-icons/bs";
 import Footer2 from "../Common/Footer2";
 import { BsBookmark } from "react-icons/bs";
+import career from "../assests/imgs/career.png";
 
 import "../../layout/EditInfo.css";
 class Appointment extends Component {
@@ -15,25 +16,37 @@ class Appointment extends Component {
         <Link to="/Profile/Activity/Appointment" />
         <div className="container">
           <ActivityNavbar setactive={"Appointment"} />
-          <div class="card mb-3">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src={rec3} class="card-img" alt="..." />
+          <div class="card mb-3 mt-3">
+            <div class="row ">
+              <div class="d-flex flex-column col-md-2 col-sm-3 col-1 d-none d-sm-flex ms-3">
+                <img
+                  className="d-flex justify-content-center align-self-center align-items-center col-md-9 col-sm-12 mt-3"
+                  src={career}
+                />
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Cv Writing</h5>
-                  <p class="card-text">Date Booked</p>
-                  <p class="card-text">Time Booked</p>
-                  <p class="card-text">
+
+              <div class="col-md-8 col-sm-7 col-8 ms-3 mt-3">
+                <div class="">
+                  <h5 class="fw-bold">Cv Writing</h5>
+                  <div className="d-flex flex-row ">
+                    <p className="me-3">Date Booked</p>
+                    <p>15 Oct 2020</p>
+                  </div>
+                  <div className="d-flex flex-row ">
+                    <p className="me-3">Time Booked</p>
+                    <p>9:00 AM</p>
+                  </div>
+                  <p class="">
                     <small id="gold">Resend this email</small>
                   </p>
                 </div>
-                <div className="  d-flex flex-row col-12 col-md-4 justify-content-end btnmovement">
-                  <button className="applyBtn px-1 py-0 col-md-3 col-8 me-2">
+              </div>
+              <div className="row mb-3">
+                <div className="d-flex justify-content-end">
+                  <button className="applyBtn col-md-2 col-sm-2 col-4  me-2">
                     Contact
                   </button>
-                  <button className="applyBtn px-1 py-0 col-md-3 col-8">
+                  <button className="applyBtn  col-md-2 col-sm-2 col-4 ">
                     Review
                   </button>
                 </div>
