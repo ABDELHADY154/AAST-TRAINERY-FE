836f4aee4484a64fb7b8b283ef832fc5a83bdf66
Yasmin <yazzmin0011@gmail.com>
2021-07-02 20:43:57 +0200

    ..

--- src/Components/Setting/Setting.jsx
+++ src/Components/Setting/Setting.jsx
@@ -24,26 +24,26 @@ class Setting extends Component {
     };
   }
   async componentDidMount() {
-      this.setState({ FormLoading: true });
-      await axios
-        .get("/W/student/studentAccount")
-        .then((res) => {
-          this.setState({
-            email: res.data.response.data.email,
-            subscribed : res.data.response.data.subscribed,
-            FormLoading: false 
-          });
-        })
-        .catch((error) => {
-          if (error.response.data.status === 401) {
-            sessionStorage.clear("token");
-            sessionStorage.clear("status");
-            this.setState({ loggedIn: false });
-            window.location.reload();
-          }
-          this.setState({ FormLoading: false });
+    this.setState({ FormLoading: true });
+    await axios
+      .get("/W/student/studentAccount")
+      .then((res) => {
+        this.setState({
+          email: res.data.response.data.email,
+          subscribed: res.data.response.data.subscribed,
+          FormLoading: false,
         });
-    
+      })
+      .catch((error) => {
+        if (error.response.data.status === 401) {
+          sessionStorage.clear("token");
+          sessionStorage.clear("status");
+          this.setState({ loggedIn: false });
+          window.location.reload();
+        }
+        this.setState({ FormLoading: false });
+      });
+
     // console.log(this.props.match.params.id);
   }
   handleupdateEmail = async (e) => {
@@ -52,16 +52,108 @@ class Setting extends Component {
     e.preventDefault();
     const data = {
       email: this.state.email1,
-  
     };
     // if (this.props.match.params.id) {
+    await axios
+      .put("/W/student/updateEmail", data)
+      .then((response) => {
+        this.setState({
+          loggedIn: true,
+          FormLoading: false,
+        });
+      })
+      .catch((error) => {
+        if (error.response.data.status === 401) {
+          sessionStorage.clear("token");
+          sessionStorage.clear("status");
+          this.setState({ loggedIn: false });
+          window.location.reload();
+        }
+        this.setState({
+          FormLoading: false,
+
+          error: {
+            emailErr: error.response.data.errors.email,
+          },
+        });
+        console.log(error);
+      });
+    // }
+  };
+  handlechangepassword = async (e) => {
+    e.preventDefault();
+    this.setState({ FormLoading: true });
+    const data = {
+      old_password: this.state.curpw,
+      password: this.state.nwpw,
+      password_confirmation: this.state.nwpw2,
+    };
+
+    await axios
+      .put("/W/student/updatePassword", data)
+      .then((response) => {
+        this.setState({
+          FormLoading: false,
+          loggedIn: true,
+        });
+      })
+      .catch((error) => {
+        if (error.response.data.status === 401) {
+          sessionStorage.clear("token");
+          sessionStorage.clear("status");
+          this.setState({ loggedIn: false });
+          window.location.reload();
+        }
+        this.setState({
+          FormLoading: false,
+          error: {
+            curpwErr: error.response.data.errors.old_password,
+            nwpwErr: error.response.data.errors.password,
+          },
+        });
+      });
+  };
+
+  handleSubscription = async (e) => {
+    this.setState({ FormLoading: true });
+    e.preventDefault();
+
+    if (this.state.subscribed == false) {
       await axios
-        .put("/W/student/updateEmail", data)
+        .get("/W/student/subscribe")
+        .then((response) => {
+          this.setState({
+            FormLoading: false,
+            subscribe: false,
+            loggedIn: true,
+          });
+          window.location.reload();
+        })
+        .catch((error) => {
+          if (error.response.data.status === 401) {
+            sessionStorage.clear("token");
+            sessionStorage.clear("status");
+            this.setState({ loggedIn: true, FormLoading: false });
+            window.location.reload();
+          }
+        });
+    }
+  };
+
+  handleunSubscription = async (e) => {
+    this.setState({ FormLoading: true });
+    e.preventDefault();
+
+    if (this.state.subscribed == true) {
+      await axios
+        .get("/W/student/unsubscribe")
         .then((response) => {
           this.setState({
             loggedIn: true,
             FormLoading: false,
+            subscribe: true,
           });
+          window.location.reload();
         })
         .catch((error) => {
           if (error.response.data.status === 401) {
@@ -72,146 +164,44 @@ class Setting extends Component {
           }
           this.setState({
             FormLoading: false,
-
-            error: {
-              emailErr: error.response.data.errors.email,
-            },
+            loggedIn: true,
           });
-          console.log(error);
         });
-    // }
-  };
-  handlechangepassword =async (e ) => {
-e.preventDefault();
-this.setState({ FormLoading: true });
-const data = {
-  old_password: this.state.curpw,
-  password: this.state.nwpw,
-  password_confirmation: this.state.nwpw2,
-
-};
-
-await axios
-.put("/W/student/updatePassword", data)
-.then((response) => {
-  this.setState({
-    FormLoading: false,
-    loggedIn: true,
-
-  });
-})
-.catch((error) => {
-  if (error.response.data.status === 401) {
-    sessionStorage.clear("token");
-    sessionStorage.clear("status");
-    this.setState({ loggedIn: false });
-    window.location.reload();
-  }
-  this.setState({
-    FormLoading: false,
-    error: {
-      curpwErr: error.response.data.errors.old_password,
-      nwpwErr: error.response.data.errors.password,
-    },
-  });
-});
-  }
-
-
-
-
-
-  handleSubscription = async (e) => {
-    this.setState({ FormLoading: true });
-    e.preventDefault();
- 
-    if (this.state.subscribed == false) {
-      await axios
-        .get("/W/student/subscribe")
-        .then((response) => {
-          this.setState({
-            FormLoading: false,
-            subscribe:false,    loggedIn: true,
-
-          });    
-        }).catch((error) => {
-          if (error.response.data.status === 401) {
-            sessionStorage.clear("token");
-            sessionStorage.clear("status");
-            this.setState({ loggedIn: true    ,         FormLoading: false,
-            });
-            window.location.reload();
-          }
-    
-        });
-        
-       
     }
-  };
-  
-  handleunSubscription = async (e) => {
-    this.setState({ FormLoading: true });
-    e.preventDefault();
- 
-    if (this.state.subscribed == true) {
-      await axios
-        .get("/W/student/unsubscribe")
-        .then((response) => {
-          this.setState({
-            loggedIn: true    ,             FormLoading: false,
-            subscribe:true,
-          });
-        }).catch((error) => {
-          if (error.response.data.status === 401) {
-            sessionStorage.clear("token");
-            sessionStorage.clear("status");
-            this.setState({ loggedIn: false  });
-            window.location.reload();
-          }
-          this.setState({
-            FormLoading: false,
-            loggedIn: true    , 
-              });
-        });
-       }
   };
   handleDelete = async (e) => {
     this.setState({ FormLoading: true });
     e.preventDefault();
-   var  data = {
+    var data = {
       password: this.state.currpw,
-      password_confirmation :  this.state.currpw,
-    }
-      await axios
-        .post("/W/student/deleteAccount", data)
-        .then((response) => {
-          this.setState({
-            loggedIn: true    ,       
-            FormLoading: false,
-            subscribe:true,
-          });
-        }).catch((error) => {
-          if (error.response.data.status === 401) {
-            sessionStorage.clear("token");
-            sessionStorage.clear("status");
-            this.setState({ loggedIn: false  });
-            window.location.reload();
-          }
-          this.setState({
-            FormLoading: false,
-            // loggedIn: true    , 
-            currpwErr : error.response.data.errors.password
-              });
+      password_confirmation: this.state.currpw,
+    };
+    await axios
+      .post("/W/student/deleteAccount", data)
+      .then((response) => {
+        this.setState({
+          loggedIn: true,
+          FormLoading: false,
+          // subscribe: true,
         });
-       
-
-    
-  }
-
-
-
-
-
+        sessionStorage.clear("token");
+        sessionStorage.clear("status");
+        this.setState({ loggedIn: false });
+        window.location.reload();
+      })
+      .catch((error) => {
+        if (error.response.data.status === 401) {
+          sessionStorage.clear("token");
+          sessionStorage.clear("status");
+          this.setState({ loggedIn: false });
+          window.location.reload();
+        }
+        this.setState({
+          FormLoading: false,
+          currpwErr: error.response.data.errors.password,
+        });
+      });
+  };
 
   handlechangepassword = async (e) => {
     this.setState({ FormLoading: true });
@@ -243,9 +233,9 @@ await axios
   };
 
   render() {
-    if (this.state.loggedIn === true) {
-      return <Redirect to="/Profile" />;
-    }
+    // if (this.state.loggedIn === true) {
+    //   return <Redirect to="/Profile" />;
+    // }
     // console.log(this.state.level);
     return (
       <div>
@@ -284,10 +274,7 @@ await axios
                         : "form-control editInput "
                     }
                     placeholder="Current Password"
-
-                    onChange={(e) =>
-                      this.setState({ curpw: e.target.value })
-                    }
+                    onChange={(e) => this.setState({ curpw: e.target.value })}
                   />
                   {this.state.error && this.state.error.curpwErr ? (
                     <p className="editerror text-capitalize">
@@ -304,11 +291,8 @@ await axios
                         ? " form-control editInput wrong"
                         : "form-control editInput "
                     }
-
                     placeholder="New Password"
-                    onChange={(e) =>
-                      this.setState({ nwpw: e.target.value })
-                    }
+                    onChange={(e) => this.setState({ nwpw: e.target.value })}
                   />
                   {this.state.error && this.state.error.nwpwErr ? (
                     <p className="editerror text-capitalize">
@@ -326,9 +310,7 @@ await axios
                         : "form-control editInput "
                     }
                     placeholder="Confirm Password  "
-                    onChange={(e) =>
-                      this.setState({ nwpw2: e.target.value })
-                    }
+                    onChange={(e) => this.setState({ nwpw2: e.target.value })}
                   />
                   {this.state.error && this.state.error.LanguageLevelErr ? (
                     <p className="editerror text-capitalize">
@@ -339,16 +321,18 @@ await axios
                   )}
                 </div>
                 <div class="mt-5 col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12 d-flex justify-content-end">
-                  <button type="submit" class="btn updateBtn shadow-none" onClick={this.handlechangepassword}>
+                  <button
+                    type="submit"
+                    class="btn updateBtn shadow-none"
+                    onClick={this.handlechangepassword}
+                  >
                     Update
                   </button>
                 </div>
               </LoadingOverlay>
             </form>
             <hr className="w-75 " />
-            <form
-              className="row g-3 mb-3 mt-1"
-            >
+            <form className="row g-3 mb-3 mt-1">
               <LoadingOverlay
                 active={this.state.FormLoading}
                 spinner={<BounceLoader color="#cd8930" />}
@@ -369,8 +353,8 @@ await axios
                   <p>
                     You are already registered with the following email:{" "}
                     <span style={{ color: "#cd8930" }} className="text-wrap">
-                     {this.state.email+ " "} 
-                    </span> 
+                      {this.state.email + " "}
+                    </span>
                     If you would like to sign-in and receive emails on a
                     different address, write this new email here:
                   </p>
@@ -394,16 +378,18 @@ await axios
                   )}
                 </div>
                 <div class="mt-5 col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12 d-flex justify-content-end">
-                  <button type="submit" class="btn updateBtn shadow-none" onClick={this.handleupdateEmail}>
+                  <button
+                    type="submit"
+                    class="btn updateBtn shadow-none"
+                    onClick={this.handleupdateEmail}
+                  >
                     Update
                   </button>
                 </div>
               </LoadingOverlay>
             </form>
             <hr className="w-75 " />
-            <form
-              className="row g-3 mb-3 mt-1"
-            >
+            <form className="row g-3 mb-3 mt-1">
               <LoadingOverlay
                 active={this.state.FormLoading}
                 spinner={<BounceLoader color="#cd8930" />}
@@ -425,53 +411,65 @@ await axios
                     You will receive emails containing the latest jobs which
                     match your preferences.
                   </p>
-             
-                    {this.state.subscribed === true ?     
+
+                  {this.state.subscribed === true ? (
                     <div className="">
- <input
-                    type="checkbox"
-                    name="inlineRadioOptions"
-                    style={{ marginBottom: 5 }}
-                    className="checkbox signInput"
-                    checked
-
-                    />
-                    <label class="form-check-label mx-2" for="inlineCheckbox3">   Receive Newsletter
-</label>
-
-
+                      <input
+                        type="checkbox"
+                        name="inlineRadioOptions"
+                        style={{ marginBottom: 5 }}
+                        className="checkbox signInput"
+                        checked
+                      />
+                      <label
+                        class="form-check-label mx-2"
+                        for="inlineCheckbox3"
+                      >
+                        {" "}
+                        Receive Newsletter
+                      </label>
                     </div>
-                                            
-                    : (
-
-<div className=""> <input
-                      type="checkbox"
-                      name="inlineRadioOptions"
-                      style={{ marginBottom: 5 }}
-                      className="checkbox signInput"
-                                            /> 
-                                                           <label class="form-check-label mx-2" for="inlineCheckbox3">   Receive Newsletter</label></div>
-                                            
-                                            
-                                            )
-                                            }
-            
+                  ) : (
+                    <div className="">
+                      {" "}
+                      <input
+                        type="checkbox"
+                        name="inlineRadioOptions"
+                        style={{ marginBottom: 5 }}
+                        className="checkbox signInput"
+                      />
+                      <label
+                        class="form-check-label mx-2"
+                        for="inlineCheckbox3"
+                      >
+                        {" "}
+                        Receive Newsletter
+                      </label>
+                    </div>
+                  )}
                 </div>
                 <div class="col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12 d-flex justify-content-end">
-                  {this.state.subscribed == true ? (   
-                       <button type="submit" class="btn updateBtn shadow-none" onClick={this.handleunSubscription}>
-                    Unsubscribe
-                  </button>
-                  ):
-                  <button type="submit" class="btn updateBtn shadow-none" onClick={this.handleSubscription}>
-                  Update
-                </button>
-                
-                }
-            
+                  {this.state.subscribed == true ? (
+                    <button
+                      type="submit"
+                      class="btn updateBtn shadow-none"
+                      onClick={this.handleunSubscription}
+                    >
+                      Unsubscribe
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      class="btn updateBtn shadow-none"
+                      onClick={this.handleSubscription}
+                    >
+                      Update
+                    </button>
+                  )}
                 </div>
               </LoadingOverlay>
-            </form>{" "} {/*
+            </form>{" "}
+            {/*
             <hr className="w-75 " />
             <form
               className="row g-3 mb-3 mt-1"
@@ -529,26 +527,27 @@ await axios
                   <input
                     type="Password"
                     className={
-                       this.state.currpwErr
+                      this.state.currpwErr
                         ? "form-control editInput wrong"
                         : "form-control editInput "
                     }
                     placeholder="Please Enter Your Password"
-                    onChange={(e) =>
-                      this.setState({ currpw: e.target.value })
-                    }
+                    onChange={(e) => this.setState({ currpw: e.target.value })}
                   />
-                       { this.state.currpwErr ? (
+                  {this.state.currpwErr ? (
                     <p className="editerror text-capitalize">
                       {this.state.currpwErr}
                     </p>
                   ) : (
                     ""
                   )}
-                  
                 </div>
                 <div class="mt-5 col-lg-10 col-11 col-md-10 col-sm-12 col-xs-12 d-flex justify-content-end">
-                  <button type="submit" class="btn deleteBtn shadow-none" onClick={this.handleDelete}>
+                  <button
+                    type="submit"
+                    class="btn deleteBtn shadow-none"
+                    onClick={this.handleDelete}
+                  >
                     Delete
                   </button>
                 </div>
